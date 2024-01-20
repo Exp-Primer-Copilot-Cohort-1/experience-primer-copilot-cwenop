a5438a7d6eb5725f3635402d90ea30a70fa08fcb
oop <61135341+cwenop@users.noreply.github.com>
2024-01-20 05:01:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,66 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'];
+    var member = {
+        name: 'Nguyen Van A',
+        age: 20,
+        skills: skills
+    };
+    console.log(member.skills);
+    console.log(member['skills']);
+    console.log(member.skills[0]);
+    console.log(member.skills[1]);
+    console.log(member.skills[2]);
+    console.log(member.skills[3]);
+    console.log(member.skills[4]);
+    console.log(member.skills[5]);
+    console.log(member.skills[6]);
+    console.log(member.skills[7]);
+    console.log(member.skills[8]);
+    console.log(member.skills[9]);
+    console.log(member.skills[10]);
+    console.log(member.skills[11]);
+    console.log(member.skills[12]);
+    console.log(member.skills[13]);
+    console.log(member.skills[14]);
+    console.log(member.skills[15]);
+    console.log(member.skills[16]);
+    console.log(member.skills[17]);
+    console.log(member.skills[18]);
+    console.log(member.skills[19]);
+    console.log(member.skills[20]);
+    console.log(member.skills[21]);
+    console.log(member.skills[22]);
+    console.log(member.skills[23]);
+    console.log(member.skills[24]);
+    console.log(member.skills[25]);
+    console.log(member.skills[26]);
+    console.log(member.skills[27]);
+    console.log(member.skills[28]);
+    console.log(member.skills[29]);
+    console.log(member.skills[30]);
+    console.log(member.skills[31]);
+    console.log(member.skills[32]);
+    console.log(member.skills[33]);
+    console.log(member.skills[34]);
+    console.log(member.skills[35]);
+    console.log(member.skills[36]);
+    console.log(member.skills[37]);
+    console.log(member.skills[38]);
+    console.log(member.skills[39]);
+    console.log(member.skills[40]);
+    console.log(member.skills[41]);
+    console.log(member.skills[42]);
+    console.log(member.skills[43]);
+    console.log(member.skills[44]);
+    console.log(member.skills[45]);
+    console.log(member.skills[46]);
+    console.log(member.skills[47]);
+    console.log(member.skills[48]);
+    console.log(member.skills[49]);
+    console.log(member.skills[50]);
+    console.log(member.skills[51]);
+    console.log(member.skills[52]);
+    console.log(member.skills[53]);
+    console.log(member.skills[54]);
+}
+    
